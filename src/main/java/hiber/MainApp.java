ad16ed7282aefbf84f13dd9b62dbe93b4b765947
Malh1n <58389533+Malh1n@users.noreply.github.com>
2021-06-05 00:56:25 +0300

    #1-#4

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -16,10 +16,10 @@ public class MainApp {
 
         UserService userService = context.getBean(UserService.class);
 
-//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-//        userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Mercedes", 666)));
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Audi", 100)));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Mazda", 3)));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("BMW", 5)));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Mercedes", 600)));
 
         List<User> users = userService.listUsers();
         for (User user : users) {
@@ -27,11 +27,26 @@ public class MainApp {
             System.out.println("First Name = " + user.getFirstName());
             System.out.println("Last Name = " + user.getLastName());
             System.out.println("Email = " + user.getEmail());
-            System.out.println("Car id = " +user.getUserCar().getCarId());
-            System.out.println("Car model = " +user.getUserCar().getModel());
-            System.out.println("Car series = " +user.getUserCar().getSeries());
+            System.out.println("Car id = " + user.getUserCar().getCarId());
+            System.out.println("Car model = " + user.getUserCar().getModel());
+            System.out.println("Car series = " + user.getUserCar().getSeries());
             System.out.println();
         }
         context.close();
     }
 }
+
+//CREATE TABLE my_database.cars (
+//  car_id int NOT NULL AUTO_INCREMENT,
+//  series int NOT NULL,
+//  model varchar(25),
+//  PRIMARY KEY (car_id));
+//
+//CREATE TABLE my_database.users (
+//  id int NOT NULL AUTO_INCREMENT,
+//  name varchar(25),
+//  last_name varchar(25),
+//  email varchar(25),
+//  car_id int,
+//  PRIMARY KEY (id)
+//, FOREIGN KEY (car_id) REFERENCES my_database.cars(car_id));
